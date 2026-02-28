8457f8e7f6c78ba23d913185295edfe88dba82e9
kgowtham007 <gk9177@srmist.edu.in>
2026-02-28 13:58:53 +0530

    An assignment given in week 01

---
+++ 01week_assignment.java
@@ -0,0 +1,10 @@
+//A program to print Harry's age
+public class 01week_assignment
+{
+  public static void main (string args[])
+    {
+	  int year =2000, current_year=2024, age;
+	  age = current year - year;
+	  System.out.println("Harry's age in " + current_year + " is " + age);
+	}
+}
