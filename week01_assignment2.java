589edef3fa296db0d32f25390823790b36e1b477
kgowtham007 <gk9177@srmist.edu.in>
2026-03-03 00:22:36 +0530

    Assignment 2 in week 01

---
+++ week01_assignment2.java
@@ -0,0 +1,13 @@
+// A program to check Sam's marks percent
+public class week01_assignment2
+{
+  public static void main (String args[])
+  {
+    int maths=94, physics = 95, chemistry =96, total = 0;
+	double average =0.0;
+	total = maths + physics + chemistry ;
+	average = total/3.0;
+	System.out.println("Sam's average mark in PCM is " + average);
+  }
+}
+	
