6e99dceec8978be470f832478e4a2fff488f6868
kgowtham007 <gk9177@srmist.edu.in>
2026-03-03 00:23:46 +0530

    Assignment 3 in week 01

---
+++ week01_assignment3.java
@@ -0,0 +1,11 @@
+// A program to convert kilometers to miles
+public class week01_assignment3
+{
+  public static void main (String args[])
+  {
+	double kilometers =10.8, miles = 0.0;
+	miles = kilometers * 1.6;
+	System.out.println("The distance " + kilometers + " km in miles is " + miles);
+  }
+}
+	
